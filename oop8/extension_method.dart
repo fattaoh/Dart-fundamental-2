571d2d4937ac0e9f982e404f5e0ea483b601724e
Fattah231-gram <fattah.alhamdani6@gmail.com>
2026-09-24 10:28:30 +0800

    feat: add extension method example with sorting functionality

---
+++ oop8/extension_method.dart
@@ -0,0 +1,30 @@
+/**
+ * Extension Method
+ * -menambahkan method serta property
+ * -bisa mengubah perilaku class
+ * -tanpa pewarisan(extends)
+ * -tanpa mixin
+ * -bisa mengubah class dari library eksternal
+ * 
+ * Syntac
+ * extension NamaExtension On ClassYangDiTuju{
+ *  
+ * }
+ * 
+ * Mobil
+ * 1. Mesin
+ * 2. Setir
+ * 3. Av
+ * 4. Radio
+ * 
+ * ==Extension Method
+ * Kamera Parking
+ * GPS
+ * Dashcam
+ */
+
+void main(List<String> args) {
+  var numbers = [2, 3, 1, 5, 4];
+
+  print(numbers);
+}
